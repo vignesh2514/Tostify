c271bbe09541b2b4b7ba818a32fdd476d913928c
vignesh2514 <vigneshmskt@gmail.com>
2018-12-04 20:47:17 +0530

    New Changes with java Code

--- tosting/src/main/java/com/vignesh2514/tosting/Ding.java
+++ tosting/src/main/java/com/vignesh2514/tosting/Ding.java
@@ -5,9 +5,10 @@ import android.widget.Toast;
 
 public class Ding {
 
-    public static void s(Context c, String message){
+    public static void s(Context c, String message) {
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+
 
     }
 
